d392f93944a2cfa540a9b0f48c36dd23b6d6a56b
Eric Eggert <mail@yatil.net>
2016-11-12 23:04:00 +0100

    include JS

---
+++ js/main.js
@@ -0,0 +1,62 @@
+var keys = {
+  tab:     9,
+  enter:  13,
+  esc:    27,
+  space:  32,
+  left:   37,
+  up:     38,
+  right:  39,
+  down:   40,
+  pgUp:   33,
+  pgDwn:  34,
+};
+
+function next() {
+  var nextels = document.querySelectorAll('.next:not(.played)');
+  if(!nextels.length) {
+    window.location.href = document.querySelector('#nextbtn').getAttribute('href');
+  } else {
+    nextels[0].classList.add('played');
+  }
+}
+
+function prev() {
+  var prevels = document.querySelectorAll('.played');
+  if(!prevels.length) {
+    window.location.href = document.querySelector('#prevbtn').getAttribute('href');
+  } else {
+    prevels[prevels.length-1].classList.remove('played');
+  }
+}
+
+document.querySelector('body').addEventListener("keydown", function(event) {
+  switch (event.keyCode) {
+    case keys.right:
+    case keys.pgDwn:
+      next();
+      event.preventDefault();
+      event.stopPropagation();
+      return false;
+      break;
+    case keys.left:
+    case keys.pgUp:
+      prev();
+      event.preventDefault();
+      event.stopPropagation();
+      return false;
+      break;
+  }
+});
+
+var more = document.querySelector('.more-info');
+
+if (more) {
+  more.addEventListener("click", function(event) {
+    document.querySelector('#content').scrollIntoView({ behavior: 'smooth' });
+  });
+}
+
+var links = document.querySelectorAll('#content a, .slide a');
+for (var i = links.length - 1; i >= 0; i--) {
+  links[i].setAttribute('target', '_blank');
+}
